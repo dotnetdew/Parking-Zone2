ffa12568ca9f429a727c132e1c46e8cba9edc99d
Abdulloh <abdullohprogram@gmail.com>
2023-12-16 18:11:12 +0500

    Added ViewModel

---
+++ ViewModels/ParkingZone/ParkingZoneDetailsVM.cs
@@ -0,0 +1,17 @@
+﻿namespace Parking_Zone.ViewModels.ParkingZone
+{
+    public class ParkingZoneDetailsVM
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfEstablishment { get; set; }
+        public ParkingZoneDetailsVM(Models.ParkingZone zone)
+        {
+            Id = zone.Id;
+            Name = zone.Name;
+            Address = zone.Address;
+            DateOfEstablishment = zone.DateOfEstablishment;
+        }
+    }
+}
